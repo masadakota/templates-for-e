ef2ab03c22dba5a94fbd5ef2f502ac6b76f6ad31
masadakota <masadakota@gmail.com>
2026-01-31 08:31:13 +0900

    Updated

--- logs/config.js
+++ logs/config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   ohinichiText: "メーカー繁忙期案内",
   defaultOhinichiText: "お日にちがかかる可能性案内",
   // datetimeFormat: "M/D HH:mm",
-  datetimeFormat: "M/D HH:■",
+  datetimeFormat: "M/D H:■",
   // datetimeFormat: "YYYY/MM/DD HH:mm"
   // operatorName: "",
 };
