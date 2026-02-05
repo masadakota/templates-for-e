adc11d287fd887f3ae6bb408f7900ca0bec4eaad
masadakota <masadakota@gmail.com>
2026-02-06 08:36:35 +0900

    Updated

--- manual/config.js
+++ manual/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  ohinichiText: "メーカー繁忙期案内", // "お日にちがかかる可能性案内" を上書きするテキスト。
+  ohinichiText: "メーカー繁忙案内", // "お日にちがかかる可能性案内" を上書きするテキスト。
   defaultOhinichiText: "お日にちがかかる可能性案内", // 日にちがかかる案内のデフォルトのテキスト。
   datetimeFormat: "M/D H:■", // 日時の形式。詳細は「YYYY-MM-DD形式」で調べてください。
   // datetimeFormat: "M/D H:mm",
